70cb3304b2901fa269b422f0cc1627483de76f65
EndocrineMSc <bamsc.studio@gmail.com>
2023-11-04 20:51:03 +0100

    Endo Girlfriend Controller

---
+++ Assets/Scripts/GirlfriendControllerEndo.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlfriendControllerEndo : MonoBehaviour
+{
+    #region Fields and Properties
+
+    private bool _isOnStation;
+    private List<Vector2> _currentRoute = new();
+    private float _currentStationTimer = 0;
+    private int _currentTargetIndex = 0;
+
+    [SerializeField] private float _speed = 3f;
+    [SerializeField] private GirlfriendStation _startStation;
+
+    #endregion
+
+    #region Methods
+
+    private void Start()
+    {
+        _currentRoute.Add(_startStation.transform.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out GirlfriendStation station))
+        {
+            Debug.Log("Collision with station!");
+            _isOnStation = true;
+            _currentStationTimer = station.StationStayDuration;
+            _currentTargetIndex = 0;
+
+            var newRoute = station.GetSortedRoute(transform);
+            _currentRoute.Clear();
+
+            foreach (var waypoint in newRoute)
+                _currentRoute.Add(waypoint.position);
+        }
+    }
+
+    private void Update()
+    {
+        if (_isOnStation)
+            _currentStationTimer -= Time.deltaTime;
+
+        if (_currentStationTimer <= 0)
+            _isOnStation = false;
+
+        if (!_isOnStation && _currentTargetIndex < _currentRoute.Count)
+        {
+            var target = _currentRoute[_currentTargetIndex];
+            var distance = Vector2.Distance(target, (Vector2)transform.position);
+            var direction = target - (Vector2)transform.position;
+
+            if (distance > 0.5f)
+                transform.Translate(direction.normalized * _speed);
+            else
+                _currentTargetIndex++;
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/GirlfriendStation.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Holds multiple viable paths for the girlfriend takes, one of which is chosen randomly to follow.
+/// Needs a trigger collider and girlfriend needs to get her path from this on collision
+/// Last waypoints in a route must end within the collider of a station!
+/// </summary>
+public class GirlfriendStation : MonoBehaviour
+{
+    #region Fields and Properties
+
+    private List<List<Transform>> ViableRoutes = new();
+    [SerializeField] private List<Transform> route1 = new();
+    [SerializeField] private List<Transform> route2 = new();
+    [field: SerializeField] public float StationStayDuration { get; private set; } = 30f;
+
+    #endregion
+
+    #region Methods
+
+    private void Start()
+    {
+        ViableRoutes.Add(route1);
+        ViableRoutes.Add(route2);
+    }
+
+    public List<Transform> GetSortedRoute(Transform girlfriendTransform)
+    {
+        List<Transform> chosenPath = new();
+
+        int randomIndex = UnityEngine.Random.Range(0, ViableRoutes.Count);
+        var unsortedRoute = ViableRoutes[randomIndex];
+
+        foreach (var waypoint in unsortedRoute)
+        {
+            var distance = Vector2.Distance(waypoint.position, girlfriendTransform.position);
+
+            if (chosenPath.Count == 0)
+            {
+                chosenPath.Add(waypoint);
+            }
+            else
+            {
+                var index = 0;
+                foreach (var point in chosenPath)
+                {
+                    var oldDistance = Vector2.Distance(point.position, girlfriendTransform.position);
+
+                    if (index == chosenPath.Count - 1) //furthest element
+                    {
+                        chosenPath.Add(waypoint);
+                        break;
+                    }
+                    else if (distance < oldDistance) //there is a farther element already
+                    {
+                        chosenPath.Insert(index, waypoint);
+                        break;
+                    }
+                    else
+                    {
+                        index++;
+                    }                  
+                }
+            }      
+        }
+
+        Debug.Log(chosenPath.Count);
+
+        return chosenPath;
+    }
+
+    #endregion
+}
