afe1dc165fa6fe67aa8861f88d1fae5fafe50d02
Esther <estherhexe@gmx.de>
2023-11-05 13:01:39 +0100

    movement detection fix versuch

--- Assets/Scripts/Ghosts/DetectMovementDirection.cs
+++ Assets/Scripts/Ghosts/DetectMovementDirection.cs
@@ -23,13 +23,19 @@ public class DetectMovementDirection : MonoBehaviour
         if (difference.x == 0.0f && difference.y == 0.0f)
         {
             animator.SetBool("moving", false);
+
+            animator.SetFloat("Move x", 0);
+
+            animator.SetFloat("Move y", 0);
         }
         else { 
             animator.SetBool("moving", true);
+            Debug.Log($"{Mathf.Abs(difference.x)}, {Mathf.Abs(difference.y)}");
+            if (Mathf.Abs(difference.x) > Mathf.Abs(difference.y))
+                animator.SetFloat("Move x", difference.x);
+            else
+                animator.SetFloat("Move y", difference.y);
         }
-           
-            animator.SetFloat("Move x", difference.x);
-            animator.SetFloat("Move y", difference.y);
 
         lastPosition = currentPosition;
 
