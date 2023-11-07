989b58be5a082aecc78b5c13f8c3da58e0be7fdb
Esther <estherhexe@gmx.de>
2023-11-07 20:17:14 +0100

    Visual Studio hat wieder Mist hinzugefügt

--- Assets/Scripts/ExorciseObject.cs
+++ Assets/Scripts/ExorciseObject.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.InputSystem.Android;
 using UnityEngine.UI;
 
 public class ExorciseObject : MonoBehaviour
